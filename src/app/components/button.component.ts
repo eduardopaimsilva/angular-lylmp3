71e8b0e0685718e73f9b8833212abcc7e52db692
eduardopaimsilva <edups.ww.com@hotmail.com>
2023-02-13 00:45:39 -0300

    Initial commit

---
+++ src/app/components/button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class Button {
+  buttonText: string = 'ACESSAR';
+  
+  @Input() label: string;
+}
